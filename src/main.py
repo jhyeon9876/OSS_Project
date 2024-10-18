33b0a6adadc03179eb414a46fab464a18ad22e1d
jhyeon <wumangboys@gmail.com>
2024-10-18 10:02:15 +0900

    Changes to be committed: 	modified:   src/main.py 	deleted:    src/test.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,34 @@
-print("Hello World!")
+from torch.autograd import Variable
+from torch.cuda import FloatTensor
+import torch.nn as nn
+
+from PIL import Image
+
+import numpy as np
+import os
+import io
+
+import argparse
+import time
+
+from math import *
+
+class DarkNet:
+    pass
+
+class Object:
+    def __init__(self):
+        self.pos_x = -1
+        self.pos_y = -1
+        self.attention_ratio = -1
+        self.image2d_weight = -1
+        self.image2d_height = -1
+
+class Car(Object):
+    pass
+
+class Human(Object):
+    pass
+
+class Server:
+    pass
